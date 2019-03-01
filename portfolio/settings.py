2d5be867f990bff91d9f4a4e9e395bdb62c4442a
GAGANsinghmsitece <gagansinghmsitece@gmail.com>
2019-03-01 10:38:55 +0530

    Releasing first version

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(locals())
